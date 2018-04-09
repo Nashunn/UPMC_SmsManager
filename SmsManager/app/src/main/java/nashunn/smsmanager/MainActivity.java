d1a0fc02a1a54f4387e3cf1a5038186f0597ba62
Nashunn <boullet.nicolas@gmail.com>
2018-04-09 18:58:41 +0200

    update for permissions

--- SmsManager/app/src/main/java/nashunn/smsmanager/MainActivity.java
+++ SmsManager/app/src/main/java/nashunn/smsmanager/MainActivity.java
@@ -1,9 +1,11 @@
 package nashunn.smsmanager;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+    private static final int SMS_PERMISSION_CODE =0 ;
     private Button btnSendSms;
     private EditText zone_phone;
     private EditText zone_msg;
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        isSmsPermissionGranted();
+        CheckSmsPermission(); //ask permission for sms
 
         btnSendSms = (Button) findViewById(R.id.buttonSend);
         zone_phone = (EditText) findViewById(R.id.inputPhone);
@@ -36,61 +38,93 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    /**
+     * Send a sms with informations from inputs on main activity
+     */
     private void sendSMS() {
         Sms sms = new Sms();
         String phone = String.valueOf(zone_phone.getText());
         String msg = String.valueOf(zone_msg.getText());
 
-        Toast.makeText(MainActivity.this, "Champs : "+phone+", "+msg, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Sms : "+phone+" -> "+msg, Toast.LENGTH_SHORT).show();
 
         if (phone != null && msg != null)
             sms.send(MainActivity.this, phone, msg);
         else
-            Toast.makeText(MainActivity.this, "Veuillez renseigner les champs", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Missing informations !", Toast.LENGTH_SHORT).show();
 
     }
 
-
     /**
-     * Check if permission for SMS is granted
+     * Check if permission for SMS is granted, if not ask for it
      */
-    public void isSmsPermissionGranted() {
+    private void CheckSmsPermission() {
         // If permission is not granted
-        if (
-            ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED
-        ) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.SEND_SMS)) {
-                // Show an explanation to the user *asynchronously*
-                // Don't block this thread waiting for the user's response
+                // Display non-blocking explanation, see more below :
                 //todo : https://developer.android.com/training/permissions/requesting.html
             }
             else {
                 // Request permission to send sms
-                ActivityCompat.requestPermissions(
-                        MainActivity.this,
-                        new String[]{Manifest.permission.SEND_SMS},
-                        MY_PERMISSIONS_REQUEST_SEND_SMS
-                );
-
+                showRequestPermissionsInfoAlertDialog(true);
             }
-        }
-        // Permission has already been granted
-        else {
-
         }
     }
 
+    /**
+     * Self explanatory
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            case SMS_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                     Toast.makeText(MainActivity.this, "Sms available", Toast.LENGTH_SHORT).show();
-                }
+                // Request permission to send sms
+                else
+                    CheckSmsPermission();
             }
         }
     }
 
+    /**
+     * Ask permission to send and read SMS
+     */
+    private void askSMSPermissions() {
+        ActivityCompat.requestPermissions(
+                MainActivity.this,
+                new String[]{Manifest.permission.SEND_SMS},
+                SMS_PERMISSION_CODE
+        );
+    }
+
+    /**
+     * Explain to the user why we need permission
+     * @param makeSystemRequestAfter
+     */
+    public void showRequestPermissionsInfoAlertDialog(final boolean makeSystemRequestAfter) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialogSmsTitle);
+        builder.setMessage(R.string.dialogSmsMsg);
+
+        builder.setPositiveButton(R.string.actionOk, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                // Display system runtime permission request if true
+                if (makeSystemRequestAfter) {
+                    askSMSPermissions();
+                }
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.show();
+    }
 
 }
